ad8569c51180395e8c5f381ced3bbca2b5f94ac9
Kiu-Kyu <minkiukiu@gmail.com>
2022-01-05 01:29:22 +0700

    add 1 class file

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,32 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = c', please enter constants:");
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = userInput.nextDouble();
+
+        System.out.print("b: ");
+        double b = userInput.nextDouble();
+
+        System.out.print("c: ");
+        double c = userInput.nextDouble();
+
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.print("Equation pass with x = " + answer);
+        } else {
+            if (b == c) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
